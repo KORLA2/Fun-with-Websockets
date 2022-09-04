90899a70e7229db09943e1be127e38806ecf8571
KORLA GOUTHAM <96729391+KORLA2@users.noreply.github.com>
2022-09-04 19:36:20 +0530

    Update Router.js

--- server/Router.js
+++ server/Router.js
@@ -2,6 +2,4 @@ let express=require('express')
 let router=express.Router();
 router.get('/',(req,res)=>{res.send('Backend Generated');})
 let cors = require("cors");
-
-
 module.exports={router,cors};
