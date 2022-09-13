34e6dfe23a562d47f0cc495c4fc1d582cf5706dc
GOUTHAM <gouthamkorla1023@gmail.com>
2022-09-13 19:19:19 +0530

    hello

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,6 +14,7 @@ const Main = () => {
       <Head>
         <h3>Korla Goutham</h3>
       
+      
   <Powers/>
         <Link
           target="_blank"
--- src/components/Particle.js
+++ src/components/Particle.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import Particles from 'react-particles'
-import {Nasa} from './particlesjs-config'
+// import  Particles from 'react-particles'
+// import {Nasa} from './particlesjs-config'
 const Particle = () => {
+
+    // console.log(Particles)
   return (
-    <div ><Particles  style={{position:'absolute',zIndex:'1'}} params={Nasa}/></div>
+     <div style={{position:'absolute'}} ></div>
   )
 }
 
